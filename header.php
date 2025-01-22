3ac326273f5ba9393c1cfa228614306d015416d2
WaterproofS0cks <marcuschanrenzhi@gmail.com>
2025-01-22 09:59:36 +0800

    Adding to repository :D

---
+++ header.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="icon" type="image/png" href="/images/favicon.ic">
+    <link rel="stylesheet" href="style.css">
+    <style>
+
+        body {
+            margin: 0;
+            padding: 0;
+            max-width: 100%;
+            overflow-x: hidden;
+        }
+       
+        .main-container {
+            position: absolute;
+            align-items: center;
+            display: flex;
+            border: 0px solid;
+            background-color:	#212121;
+            width: 100%;
+            height: 80px;
+            padding: 0 10px;
+        }
+
+        #logo {
+            position: relative;
+            height: 55px;
+            width: auto;
+            padding: 3px;
+            margin: 3px;
+        }
+
+        .main-container input {
+            position: relative;  
+            height: 30px;
+            width: 45%;
+            border-radius: 8px;
+            left: 13%;
+            font-size: 12px;
+        }
+
+        .main-container input::placeholder {
+            font-size: 16px;
+            text-align: center;
+        }
+
+        #pfp {
+            position: relative;
+            height: 55px;
+            width: 55px;
+            padding: 30px;
+            right: 0;
+            margin-left: auto;
+
+        }
+
+
+    </style>
+</head>
+<header>
+    <div class="main-container">
+
+            
+
+            <a href="home.php" class="font-regular"><img src="img/APNet_Logo.png" alt="APNet Logo" id="logo"></a>
+
+            <input type="text" name="" id="" placeholder="Search APNet..." class="font-regular">
+
+            <img src="img/Default_pfp.png" alt="APNet Logo" id="pfp">
+
+    </div>
+</header>
+<body>
+</body>
+</html>
