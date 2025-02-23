751f983108deea93ef5b7f80b8be15879a4631c4
Karlson Thien <prolexicgt@gmail.com>
2025-02-23 14:52:38 +0800

    Updated profile, register, login to inherit headers. Added login and register functions with connection to database

---
+++ auth.py
@@ -0,0 +1,53 @@
+from flask import Blueprint, render_template, request, url_for, redirect, Response
+import psycopg2
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
+DBNAME = os.getenv("DBNAME")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+
+auth = Blueprint("auth", __name__, static_folder="static", template_folder="templates")
+
+@auth.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        client = request.form
+        user = (client['email'], client['password'])
+        conn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD)
+        cur = conn.cursor()
+        try:
+            cur.execute("SELECT EXISTS (SELECT 1 FROM Users WHERE email = %s AND password = %s)", user)
+            check = cur.fetchone()
+            print(check)
+            print(type(check))
+            conn.close()
+            if check[0]:
+                return redirect(url_for('profile.view_profile'))
+            else:
+                return Response(response="Username or email incorrect", status=401)
+        except Exception as e:
+            return Response(response=f"Username or email already exists. {e}", status=400)
+    else:
+        return render_template("login.html")
+    
+@auth.route('/register', methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        client = request.form
+        date = datetime.now().strftime('%Y-%m-%d')
+        user = (client['username'], client['fullname'], 'U', client['email'], client["password"], client["gender"], date)
+        conn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD)
+        cur = conn.cursor()
+        try:
+            cur.execute("INSERT INTO Users (username, fullname, role, email, password, gender, RegisterDate, LastLogin, ProfilePicture) VALUES (%s, %s, %s, %s, %s, %s, %s, NULL, NULL)", user)          
+            conn.commit()
+            conn.close()
+            return redirect(url_for('profile.view_profile'))
+        except psycopg2.errors.UniqueViolation as e:
+            return Response(response=f"Username or email already exists. {e}", status=400)
+    else:
+        return render_template("register.html")
+    
--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template, url_for, request, Response
+from flask import Flask, render_template, Response
+from auth import auth
+from user_profile import profile
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DBNAME = os.getenv("DBNAME")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
 
 app = Flask(__name__)
+app.register_blueprint(auth, url_prefix="/auth")
+app.register_blueprint(profile, url_prefix="/user")
 
-@app.route('/login', methods=["POST","GET"])
-def login():
-    return render_template("login.html")
+conn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD)
+cur = conn.cursor()
+
+cur.execute("""CREATE TABLE IF NOT EXISTS Users (
+            id SERIAL NOT NULL PRIMARY KEY,
+            username VARCHAR(255) UNIQUE NOT NULL,
+			fullname VARCHAR (255) NOT NULL,
+            role CHAR(1) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            gender CHAR(1),
+			RegisterDate DATE NOT NULL,
+			LastLogin TIMESTAMP,
+			ProfilePicture VARCHAR(255)
+            );
+""")
+
+conn.commit()
+conn.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
