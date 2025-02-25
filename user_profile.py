15d3f5fced2fad02520888047f0963acc60ae905
Java <yewshan.ooi@gmail.com>
2025-02-25 13:33:54 +0800

    small changes 2

--- user_profile.py
+++ user_profile.py
@@ -14,7 +14,7 @@ user_profile = Blueprint("user_profile", __name__, static_folder="static", templ
 def profile():
     if "user" in session:
         if request.method == "GET":
-            return render_template("profile.html", name = session.get('user'), pfp = session['pfp'], bio = session['bio'])
+            return render_template("profile.html", name = session.get('user'), pfp = session.get('pfp'), bio = session.get('bio'), link = session.get('link'))
     else:
         return redirect(url_for('auth.login'))
     
