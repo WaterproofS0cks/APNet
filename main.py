a7d8fd92655c8e70716dea91ed7b787446d536bc
Java <yewshan.ooi@gmail.com>
2025-02-25 12:43:10 +0800

    feat: changed button to external link instead

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ cur.execute("""CREATE TABLE IF NOT EXISTS Users (
             username VARCHAR(255) UNIQUE NOT NULL,
 			fullname VARCHAR(255) NOT NULL,
             bio VARCHAR(255),
+            link VARCHAR(255),
             role CHAR(1) NOT NULL,
             email VARCHAR(255) UNIQUE NOT NULL,
             password VARCHAR(255) NOT NULL,
